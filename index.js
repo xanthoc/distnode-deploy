f5271e28404a1a5bd3060b5f702fa080bc05cc41
Cheolju Hwang <cheolju.hwang@samsung.com>
2021-12-15 01:12:15 +0000

    app files

---
+++ index.js
@@ -0,0 +1,18 @@
+import fastify from "fastify";
+import { Recipe } from "./recipe.js";
+
+const HOST = process.env.HOST || "127.0.0.1";
+const PORT = process.env.PORT || 8000;
+
+const server = fastify();
+server.get("/", async (req, reply) => {
+  return "Hello from Distributed Node.js!";
+});
+server.get("/recipe/:id", async (req, reply) => {
+  const recipe = new Recipe(req.params.id);
+  await recipe.hydrate();
+  return recipe;
+});
+server.listen(PORT, HOST, (err, host) => {
+  console.log(`Server running at ${host}`);
+});
